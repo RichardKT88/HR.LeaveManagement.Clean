082b7589f24f3d912dd847928925ca1b00fa73a7
RichardKT88 <kendy_t88@hotmail.com>
2024-01-24 15:15:13 -0300

    End of section 5

--- HR.LeaveManagement.Application/Features/Command/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/Command/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Logging;
 using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
 using HR.LeaveManagement.Domain;
 using MediatR;
 
@@ -9,21 +11,28 @@ public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeComm
 {
     private readonly IMapper _mapper;
     private readonly ILeaveTypeRepository _leaveTypeRepository;
+    private readonly IAppLogger<UpdateLeaveTypeCommandHandler> _logger;
 
-    public UpdateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    public UpdateLeaveTypeCommandHandler(IMapper mapper, 
+        ILeaveTypeRepository leaveTypeRepository,
+        IAppLogger<UpdateLeaveTypeCommandHandler> logger)
     {
         _mapper = mapper;
         _leaveTypeRepository = leaveTypeRepository;
+        _logger = logger;
     }
 
     public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
     {
         // Validate incoming data
-        //var validator = new UpdateLeaveTypeCommandValidator(_leaveTypeRepository);
-        //var validationResult = await validator.ValidateAsync(request);
+        var validator = new UpdateLeaveTypeCommandValidator(_leaveTypeRepository);
+        var validationResult = await validator.ValidateAsync(request);
 
-       /* if (validationResult.Errors.Any())
-            throw new BadRequestException("Invalid Leave type", validationResult);*/
+        if (validationResult.Errors.Any())
+        {
+            _logger.LogWarning("Validation errors in update request for {0} - {1}", nameof(LeaveType), request.Id);
+            throw new BadRequestException("Invalid Leave type", validationResult);
+        }
 
         // convert to domain entity object
         var leaveTypeToUpdate = _mapper.Map<LeaveType>(request);
