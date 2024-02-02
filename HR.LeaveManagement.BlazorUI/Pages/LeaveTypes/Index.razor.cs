93bc0321d7643231ec268f1e486f1f27c0a5595b
RichardKT88 <kendy_t88@hotmail.com>
2024-02-02 17:05:45 -0300

    End of section 11

--- HR.LeaveManagement.BlazorUI/Pages/LeaveTypes/Index.razor.cs
+++ HR.LeaveManagement.BlazorUI/Pages/LeaveTypes/Index.razor.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast.Services;
 using HR.LeaveManagement.BlazorUI.Contracts;
 using HR.LeaveManagement.BlazorUI.Models.LeaveTypes;
 using Microsoft.AspNetCore.Components;
@@ -13,8 +14,8 @@ public partial class Index
     public ILeaveTypeService LeaveTypeService { get; set; }
     [Inject]
     public ILeaveAllocationService LeaveAllocationService { get; set; }
-    //[Inject]
-    //IToastService toastService { get; set; }
+    [Inject]
+    IToastService toastService { get; set; }
     public List<LeaveTypeVM> LeaveTypes { get; private set; }
     public string Message { get; set; } = string.Empty;
 
@@ -44,7 +45,7 @@ public partial class Index
         var response = await LeaveTypeService.DeleteLeaveType(id);
         if (response.Success)
         {
-            //toastService.ShowSuccess("Leave Type deleted Successfully");
+            toastService.ShowSuccess("Leave Type deleted Successfully");
             await OnInitializedAsync();
         }
         else
